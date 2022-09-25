297f5b5fd43e5be1fc05d533a03e8b48cf058e41
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-09-25 08:46:29 -0400

    task #1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryImagesItems = createImagesMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryImagesItems);
+
+function createImagesMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </div>
+`;
+    })
+    .join('');
+}
+
+gallery.addEventListener('click', onGalleryItemsClick);
+
+function onGalleryItemsClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const activeImages = event.target;
+  const instance = basicLightbox.create(
+    `
+      <div class="modal">
+          <img src="${activeImages.dataset.source}"/>
+          
+      </div>
+    `
+  );
+  instance.show();
+
+  document.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  });
+}
 
 console.log(galleryItems);
